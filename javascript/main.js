2d08ca69c76f5bc79addf9b98c1f12fdb1afb615
Joyanta Kumar <joyroy122004@gmail.com>
2025-08-05 13:05:05 +0600

    Atleast the addRanomNode and addRandomEdge works

--- javascript/main.js
+++ javascript/main.js
@@ -4,19 +4,46 @@ import { Graph } from "./math/graph.js"
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const n1 = new Node(100, 100);
-const n2 = new Node(300, 100);
-const n3 = new Node(400, 300);
-const n4 = new Node(200, 500);
-const e1 = new Edge(n1, n2);
-const e2 = new Edge(n2, n3);
-const e3 = new Edge(n3, n4);
-const e4 = new Edge(n2, n4)
-const graph = new Graph([n1, n2, n3, n4], [e1, e2, e3, e4]);
-graph.draw(ctx);
+
+const graph = new Graph([],[]);
 
 
+function addRandomNode() {
+  graph.tryAddNode(new Node(
+    Math.floor(Math.random() * canvas.width),
+    Math.floor(Math.random() * canvas.height)
+  ));
+  console.log(graph.nodes.length, graph.edges.length);
+}
+
+
+function addRandomEdge() {
+  const startIndex = Math.floor(Math.random() * graph.nodes.length);
+  const endIndex = Math.floor(Math.random() * graph.nodes.length);
+  if (startIndex == endIndex) {
+    return;
+  }
+  graph.tryAddEdge(new Edge(
+    graph.nodes[startIndex],
+    graph.nodes[endIndex]
+  ));
+  console.log(graph.nodes.length, graph.edges.length);
+}
+
+// Making things globally available
+window.canvas = canvas;
+window.ctx = ctx;
+window.addRandomNode = addRandomNode;
+window.addRandomEdge = addRandomEdge;
+
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  graph.draw(ctx);
+  requestAnimationFrame(animate)
+}
+
+requestAnimationFrame(animate);
--- javascript/math/graph.js
+++ javascript/math/graph.js
@@ -12,4 +12,32 @@ export class Graph {
       element.draw(ctx);
     }
   }
+
+  containsNode(node) {
+    return this.nodes.find(n => node.equals(n))
+  }
+
+  containsEdge(edge) {
+    return this.edges.find(e => edge.equals(e))
+  }
+
+  addNode(node) {
+    this.nodes.push(node);
+  }
+
+  tryAddNode(node) {
+    if (this.containsNode(node) == undefined) {
+      this.addNode(node);
+    }
+  }
+
+  addEdge(edge) {
+    this.edges.push(edge);
+  }
+
+  tryAddEdge(edge) {
+    if (this.containsEdge(edge) == undefined) {
+      this.addEdge(edge);
+    }
+  }
 }
